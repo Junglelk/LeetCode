c750d789a9bf9ec429f7c043c727903c21ff4c71
jungle <1198581227@qq.com>
2021-07-19 14:29:48 +0800

    0121 db

--- src/question_0121/BestTimeToBuyAndSellStock/Solution.java
+++ src/question_0121/BestTimeToBuyAndSellStock/Solution.java
@@ -27,10 +27,13 @@ import org.junit.Test;
  */
 public class Solution {
     @Test
-    public void test(){
-        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    public void test() {
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfitViolence(new int[]{7,1,5,3,6,4}));
     }
-    public int maxProfit(int[] prices) {
+
+    public int maxProfitViolence(int[] prices) {
         // 暴力求解
         int max = 0;
         for (int i = 0; i < prices.length; i++) {
@@ -40,4 +43,19 @@ public class Solution {
         }
         return max;
     }
+
+    public int maxProfit(int[] prices) {
+        // 假设我们当前的为最小值，寻找当前之后的所有值中的最大值
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else if (max < price - min) {
+                max = price - min;
+            }
+        }
+        return max;
+    }
+
 }
