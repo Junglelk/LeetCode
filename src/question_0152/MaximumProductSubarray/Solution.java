ae6d619e28a252c6ccb36d96b854927d9c8b35bd
Jungle <1198581227@qq.com>
2021-07-21 21:14:10 +0800

    0152

---
+++ src/question_0152/MaximumProductSubarray/Solution.java
@@ -0,0 +1,45 @@
+package question_0152.MaximumProductSubarray;
+
+import org.junit.Test;
+
+/**
+ * Description:
+ * <p>
+ * Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product,
+ * and return the product.
+ * It is guaranteed that the answer will fit in a 32-bit integer.
+ * A subarray is a contiguous subsequence of the array.
+ * </p>
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ * </p>
+ * <p>
+ * <pre>
+ *  输入: [2,3,-2,4]
+ *  输出: 6
+ *  解释: 子数组 [2,3] 有最大乘积 6。
+ * </pre>
+ * <pre>
+ *  输入: [-2,0,-1]
+ *  输出: 0
+ *  解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ * </pre>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-product-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </p>
+ *
+ * @author Jungle
+ * @since 2021/7/21 14:54
+ */
+public class Solution {
+    @Test
+    public void test() {
+        System.out.println(maxProduct(new int[]{-1, -2, -6}));
+    }
+
+    public int maxProduct(int[] nums) {
+
+
+        return 0;
+    }
+}
