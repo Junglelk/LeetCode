e82f693dda6582dfd23c2a3e7c72ef56128b9a17
Jungle <1198581227@qq.com>
2021-07-31 15:44:41 +0800

    1035 提取变量

--- src/question_1035/UncrossedLines/Solution.java
+++ src/question_1035/UncrossedLines/Solution.java
@@ -55,10 +55,12 @@ public class Solution {
 
 
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
-        for (int i = 1; i <= nums1.length; i++) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
             int temp = nums1[i - 1];
-            for (int j = 1; j <= nums2.length; j++) {
+            for (int j = 1; j <= n; j++) {
                 if (temp == nums2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
@@ -66,6 +68,6 @@ public class Solution {
                 }
             }
         }
-        return dp[nums1.length][nums2.length];
+        return dp[m][n];
     }
 }
