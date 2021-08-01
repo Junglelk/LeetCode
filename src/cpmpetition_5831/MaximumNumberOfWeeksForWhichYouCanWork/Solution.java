88a711f16d6a778a279d2bb2fe32b54722a3ca21
Jungle <1198581227@qq.com>
2021-08-01 11:28:57 +0800

    竞赛-5831

---
+++ src/cpmpetition_5831/MaximumNumberOfWeeksForWhichYouCanWork/Solution.java
@@ -0,0 +1,31 @@
+package cpmpetition_5831.MaximumNumberOfWeeksForWhichYouCanWork;
+
+/**
+ * 给你 n 个项目，编号从 0 到 n - 1 。同时给你一个整数数组 milestones ，其中每个 milestones[i] 表示第 i 个项目中的阶段任务数量。
+ * <p>
+ * 你可以按下面两个规则参与项目中的工作：
+ * <p>
+ * 1.每周，你将会完成 某一个 项目中的 恰好一个 阶段任务。你每周都 必须 工作。<br>
+ * 2.在 连续的 两周中，你 不能 参与并完成同一个项目中的两个阶段任务。<br>
+ * 一旦所有项目中的全部阶段任务都完成，或者仅剩余一个阶段任务都会导致你违反上面的规则，那么你将 停止工作 。注意，由于这些条件的限制，你可能无法完成所有阶段任务。
+ * <p>
+ * 返回在不违反上面规则的情况下你 最多 能工作多少周。
+ *
+ * @author Jungle
+ * @since 2021/8/1 10:54
+ */
+public class Solution {
+    /**
+     * <p>
+     * 1.每周，你将会完成 某一个 项目中的 恰好一个 阶段任务。你每周都 必须 工作。<br>
+     * 2.在 连续的 两周中，你 不能 参与并完成同一个项目中的两个阶段任务。<br>
+     * </p>
+     *
+     * @param milestones 整数数组 milestones,milestones[i] 表示第 i 个项目中的阶段任务数量
+     * @return 最大工作周数
+     */
+    public long numberOfWeeks(int[] milestones) {
+
+        return 0L;
+    }
+}
