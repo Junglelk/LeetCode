02d48d4dc1d33e5ede420137788d5cd437d93937
Jungle <1198581227@qq.com>
2021-08-02 23:10:26 +0800

    0712

--- src/question_0712/MinimumASCIIDeleteSumForTwoStrings/Solution.java
+++ src/question_0712/MinimumASCIIDeleteSumForTwoStrings/Solution.java
@@ -2,9 +2,6 @@ package question_0712.MinimumASCIIDeleteSumForTwoStrings;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 给定两个字符串s1, s2，找到使两个字符串相等所需删除字符的ASCII值的最小和。
  *
@@ -26,20 +23,21 @@ public class Solution {
         int m = s1.length();
         int n = s2.length();
         int[][] dp = new int[m + 1][n + 1];
-        List<Character> equal = new ArrayList<>();
-        for (int i = 1; i <= s1.length(); i++) {
-            int temp = s1.charAt(i - 1);
-            for (int j = 1; j <= s2.length(); j++) {
-                int temp1 = s2.charAt(j - 1);
-                if (temp1 == temp) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                    equal.add((char)temp);
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            dp[i][s2.length()] = dp[i + 1][s2.length()] + s1.codePointAt(i);
+        }
+        for (int j = s2.length() - 1; j >= 0; j--) {
+            dp[s1.length()][j] = dp[s1.length()][j + 1] + s2.codePointAt(j);
+        }
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(dp[i + 1][j] + s1.codePointAt(i), dp[i][j + 1] + s2.codePointAt(j));
                 }
             }
         }
-        System.out.println(equal);
-        return dp[m][n];
+        return dp[0][0];
     }
 }
