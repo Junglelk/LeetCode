61541ec7b1c0c2da6b16f09803a8d1360a750d67
Jungle <1198581227@qq.com>
2021-08-03 22:15:47 +0800

    第二天再写 我的水平，垃圾透顶

--- src/question_0300/LongestIncreasingSubsequence/Solution.java
+++ src/question_0300/LongestIncreasingSubsequence/Solution.java
@@ -37,19 +37,19 @@ public class Solution {
         if (nums.length == 1) {
             return 1;
         }
-        // 由于子序列可以有任意的组合，所以在每一步都需要返回最初始处重新计算到当前位置处的最优解
-        int[] dp = new int[nums.length];
         int max = 1;
-        dp[0] = 1;
+        int[] dp = new int[nums.length];
         for (int i = 1; i < nums.length; i++) {
-            // 需要对新到达的位置赋值，赋值为 1 为到当前位置处连续增长最大子序列为 1 即为nums[i] 本身，这样才有比较的基础
+            int temp = nums[i - 1];
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                int temp1 = nums[j];
+                if (temp > temp1) {
+                    dp[i] = Math.max(dp[i - 1] + 1, dp[i]);
+                } else {
+                    dp[i] = dp[i - 1];
                 }
             }
-            max = Math.max(dp[i], max);
         }
         return max;
     }
