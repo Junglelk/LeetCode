aa331155853e8ff6a350c52018a9e3190d8afbf8
Jungle <1198581227@qq.com>
2021-08-06 15:29:26 +0800

    应该是双指针，不是dp

---
+++ src/question_0392/IsSubsequence/Solution.java
@@ -0,0 +1,44 @@
+package question_0392.IsSubsequence;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Description:
+ * <p>
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ * </p>
+ *
+ * @author Jungle
+ * @since 2021/8/6 11:43
+ */
+public class Solution {
+    @Test
+    public void test() {
+        System.out.println(isSubsequence("", "abcde"));
+    }
+
+    public boolean isSubsequence(String s, String t) {
+        if ("".equals(s)){
+            return true;
+        }
+        if (t.length() <= s.length()) {
+            return s.equals(t);
+        }
+        boolean[] dp = new boolean[s.length()];
+        Arrays.fill(dp, false);
+        int j = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (j == s.length()) {
+                break;
+            }
+            if (t.charAt(i) == s.charAt(j)) {
+                dp[j] = true;
+                j++;
+            }
+        }
+        return dp[s.length()-1];
+    }
+}
