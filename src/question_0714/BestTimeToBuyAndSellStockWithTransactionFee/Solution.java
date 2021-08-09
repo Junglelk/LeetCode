05dd34163a469c0b5fdbf62f07053e73e93715cf
Jungle <1198581227@qq.com>
2021-08-09 14:10:48 +0800

    复习买卖股票带手续费问题

--- src/question_0714/BestTimeToBuyAndSellStockWithTransactionFee/Solution.java
+++ src/question_0714/BestTimeToBuyAndSellStockWithTransactionFee/Solution.java
@@ -42,8 +42,8 @@ public class Solution {
     @Test
     public void test() {
         // 这个数组下，答案是 8
-        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9},2));
-        System.out.println(maxProfitG(new int[]{1, 3, 2, 8, 4, 9},2));
+        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(maxProfitG(new int[]{1, 3, 2, 8, 4, 9}, 2));
     }
 
     public int maxProfit(int[] prices, int fee) {
@@ -56,13 +56,14 @@ public class Solution {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
         }
-        return dp[n-1][0];
+        return dp[n - 1][0];
     }
 
     /**
      * 优化空间复杂度
+     *
      * @param prices 价格数组
-     * @param fee 交易费
+     * @param fee    交易费
      * @return Integer
      */
     public int maxProfitG(int[] prices, int fee) {
@@ -71,6 +72,27 @@ public class Solution {
         for (int i = 1; i < n; ++i) {
             sell = Math.max(sell, buy + prices[i] - fee);
             buy = Math.max(buy, sell - prices[i]);
+        }
+        return sell;
+    }
+
+    public int maxProfitRe(int[] prices, int fee) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+        }
+        return dp[prices.length - 1][0];
+    }
+
+    public int maxProfitRePolish(int[] prices, int fee) {
+        int sell = 0;
+        int buy = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            sell = Math.max(buy + prices[i] - fee, sell);
+            buy = Math.max(sell - prices[i], buy);
         }
         return sell;
     }
