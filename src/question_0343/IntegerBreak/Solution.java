48ec52c149ca40db8c482946b6e03f6e7db39893
Jungle <1198581227@qq.com>
2021-08-18 23:39:59 +0800

    在装机 水一下

--- src/question_0343/IntegerBreak/Solution.java
+++ src/question_0343/IntegerBreak/Solution.java
@@ -14,6 +14,7 @@ import org.junit.Test;
  * 输出: 36
  * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
  * </pre>
+ *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/integer-break
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
