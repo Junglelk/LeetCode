7df50e6baff5084b893493c1365fca0a91c9403f
Jungle <1198581227@qq.com>
2021-08-22 11:46:56 +0800

    5852 搞不定

---
+++ src/competition_5852/MinimizeTheDifferenceBetweenTargetAndChosenElements/Solution.java
@@ -0,0 +1,29 @@
+package competition_5852.MinimizeTheDifferenceBetweenTargetAndChosenElements;
+
+/**
+ * 给你一个大小为 m x n 的整数矩阵 mat 和一个整数 target 。
+ * <p>
+ * 从矩阵的 每一行 中选择一个整数，你的目标是 最小化 所有选中元素之 和 与目标值 target 的 绝对差 。
+ * <p>
+ * 返回 最小的绝对差 。
+ * <p>
+ * a 和 b 两数字的 绝对差 是 a - b 的绝对值。
+ * <pre>
+ * 输入：mat = [[1,2,3],[4,5,6],[7,8,9]], target = 13
+ * 输出：0
+ * 解释：一种可能的最优选择方案是：
+ * - 第一行选出 1
+ * - 第二行选出 5
+ * - 第三行选出 7
+ * 所选元素的和是 13 ，等于目标值，所以绝对差是 0 。
+ * </pre>
+ *
+ * @author Jungle
+ * @since 2021/8/22 10:53
+ */
+public class Solution {
+    public int minimizeTheDifference(int[][] mat, int target) {
+
+        return 0;
+    }
+}
