9a52008d5f7677789eb16aebf091e47bd5dd9b0d
Jungle <1198581227@qq.com>
2021-09-01 22:29:55 +0800

    0091-困且累，明天写题解

---
+++ src/question_0091/DecodeWays/Solution.java
@@ -0,0 +1,78 @@
+package question_0091.DecodeWays;
+
+import org.junit.Test;
+
+/**
+ * A message containing letters from A-Z can be encoded into numbers using the following mapping:
+ * <pre>
+ * 'A' -> "1"
+ * 'B' -> "2"
+ * ...
+ * 'Z' -> "26"
+ * </pre>
+ * To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+ * <pre>
+ * "AAJF" with the grouping (1 1 10 6)
+ * "KJF" with the grouping (11 10 6)
+ * </pre>
+ * Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+ * <p>
+ * Given a string s containing only digits, return the number of ways to decode it.
+ * <p>
+ * The answer is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * 一条包含字母 A-Z 的消息通过以下映射进行了 编码 ：
+ * <pre>
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ *  </pre>
+ * 要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+ * <p>
+ * <pre>
+ * "AAJF" ，将消息分组为 (1 1 10 6)
+ * "KJF" ，将消息分组为 (11 10 6)
+ * </pre>
+ * <p>
+ * 注意，消息不能分组为  (1 11 06) ，因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+ * <p>
+ * 给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+ * <p>
+ * 题目数据保证答案肯定是一个 32 位 的整数。
+ * <pre>
+ * 输入：s = "12"
+ * 输出：2
+ * 解释：它可以解码为 "AB"（1 2）或者 "L"（12）。
+ * </pre>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/decode-ways
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/9/1 21:18
+ */
+public class Solution {
+
+    @Test
+    public void test() {
+        System.out.println(numDecodings("101101111111"));
+    }
+
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
+}
