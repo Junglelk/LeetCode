4b47ebefda45a5b466dffd928ae334b0b30796c6
Jungle <1198581227@qq.com>
2021-10-01 10:29:20 +0800

    0035-解过的题

---
+++ src/question_0035/SearchInsertPosition/Solution.java
@@ -0,0 +1,32 @@
+package question_0035.SearchInsertPosition;
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/10/1 10:27
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1, ans = n;
+        while (low <= high) {
+            int middle = (high+low) >> 1;
+            if (target <= nums[middle]) {
+                ans = middle;
+                high = middle - 1;
+            }else {
+                low = middle + 1;
+            }
+        }
+        return ans;
+    }
+}
