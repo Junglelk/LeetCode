d7cce119619ac4ede0bd65c305fb7ab4e6396225
Jungle <1198581227@qq.com>
2021-10-08 22:17:29 +0800

    还是超时间复杂度

--- src/question_0387/FirstUniqueCharacterInAString/Solution.java
+++ src/question_0387/FirstUniqueCharacterInAString/Solution.java
@@ -17,17 +17,11 @@ public class Solution {
     public int firstUniqChar(String s) {
         Map<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
-            } else {
-                map.put(c, 1);
-            }
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         for (int i = 0; i < s.toCharArray().length; i++) {
-            if (map.containsKey(s.charAt(i))) {
-                if (map.get(s.charAt(i)) == 1) {
-                    return i + 1;
-                }
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
             }
         }
         return -1;
