b0fe3a9a63f3001e08145a16a38f3fe11ce69797
Jungle <1198581227@qq.com>
2021-10-09 21:57:40 +0800

    用栈模拟队列

---
+++ src/question_0232/ImplementQueueUsingStacks/Solution.java
@@ -0,0 +1,78 @@
+package question_0232.ImplementQueueUsingStacks;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+ * <p>
+ * Implement the MyQueue class:
+ * <p>
+ * void push(int x) Pushes element x to the back of the queue.
+ * int pop() Removes the element from the front of the queue and returns it.
+ * int peek() Returns the element at the front of the queue.
+ * boolean empty() Returns true if the queue is empty, false otherwise.
+ * Notes:
+ * <p>
+ * You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+ * Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ * <p>
+ * 实现 MyQueue 类：
+ * <p>
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ *  
+ * <p>
+ * 说明：
+ * <p>
+ * 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/10/9 21:47
+ */
+public class Solution {
+    class MyQueue {
+        Deque<Integer> inStack;
+        Deque<Integer> outStack;
+
+        public MyQueue() {
+            inStack = new LinkedList<>();
+            outStack = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop() {
+            if (outStack.isEmpty()) {
+                in2out();
+            }
+            return outStack.pop();
+        }
+
+        public int peek() {
+            if (outStack.isEmpty()) {
+                in2out();
+            }
+            return outStack.peek();
+        }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+        }
+
+        private void in2out() {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+}
