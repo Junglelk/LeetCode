388466786e8be6ee04e045cdbc829ead789f26a4
Jungle <1198581227@qq.com>
2021-10-09 14:58:31 +0800

    0141

---
+++ src/other/ListNode.java
@@ -0,0 +1,20 @@
+package other;
+
+/**
+ * Description:
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jungle
+ * @since 2021/10/9 14:17
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
