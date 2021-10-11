3b460ffbe3242991cb6d6d29881a7c9df32d7cc7
Jungle <1198581227@qq.com>
2021-10-11 22:54:23 +0800

    0104

---
+++ src/question_0104/MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,37 @@
+package question_0104.MaximumDepthOfBinaryTree;
+
+import other.TreeNode;
+
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ * <p>
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * 给定一个二叉树，找出其最大深度。
+ * <p>
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/10/11 22:19
+ */
+public class Solution {
+    /**
+     * dfs 深度优先搜索
+     *
+     * @param root 根节点
+     * @return 深度
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
