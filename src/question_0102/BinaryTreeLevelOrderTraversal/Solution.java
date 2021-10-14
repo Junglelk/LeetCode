1b2622f912f8d47ba18407f587b11ff915079d6a
Jungle <1198581227@qq.com>
2021-10-14 21:22:26 +0800

    0102-错解-使用变量作为遍历的基数

--- src/question_0102/BinaryTreeLevelOrderTraversal/Solution.java
+++ src/question_0102/BinaryTreeLevelOrderTraversal/Solution.java
@@ -1,4 +1,5 @@
 package question_0102.BinaryTreeLevelOrderTraversal;
+
 import other.TreeNode;
 
 import java.util.ArrayList;
@@ -7,8 +8,14 @@ import java.util.List;
 import java.util.Queue;
 
 public class Solution {
+    /**
+     * 二叉树的层序遍历
+     *
+     * @param root 根节点
+     * @return 遍历结果
+     */
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root==null) {
+        if (root == null) {
             return new ArrayList<>();
         }
         List<List<Integer>> list = new ArrayList<>();
@@ -16,15 +23,15 @@ public class Solution {
         queue.add(root);
         while (!queue.isEmpty()){
             List<Integer> temp = new ArrayList<>();
-            int qs = queue.size();
-            for (int i = 0; i < qs; i++) {
-                TreeNode head = queue.poll();
-                temp.add(head.val);
-                if (head.left!=null){
-                    queue.add(head.left);
+            // 这里不能使用queue.size() 因为下面这个的长度会变化
+            for (int i = 0; i < queue.size(); i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
                 }
-                if (head.right!=null){
-                    queue.add(head.right);
+                if (node.right != null) {
+                    queue.add(node.right);
                 }
             }
             list.add(temp);
