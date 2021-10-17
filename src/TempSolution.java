5c1f131a365737bba2fa3e4f503788bd65563e0f
Jungle <1198581227@qq.com>
2021-10-17 15:03:03 +0800

    5902

--- src/TempSolution.java
+++ src/TempSolution.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Jungle
@@ -12,27 +10,9 @@ public class TempSolution {
 
     @Test
     public void test() {
-        System.out.println(areNumbersAscending("1 box has 3 blue 4 red 6 green and 12 yellow marbles"));
+        System.out.println();
     }
 
-
-    public boolean areNumbersAscending(String s) {
-        List<Integer> list = new ArrayList<>();
-        String[] ret = s.split(" ");
-        for (String s1 : ret) {
-            if (Character.isDigit(s1.charAt(0))) {
-                list.add(Integer.parseInt(s1));
-            }
-        }
-
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) >= list.get(i + 1)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 
     public int countMaxOrSubsets(int[] nums) {
 
