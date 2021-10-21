64ff8aeed4d3e09202b0384db1b7a28cfa3a93f1
Jungle <1198581227@qq.com>
2021-10-21 22:48:04 +0800

    0097-不会！看不懂！

--- src/question_0097/InterleavingString/Solution.java
+++ src/question_0097/InterleavingString/Solution.java
@@ -45,11 +45,32 @@ import org.junit.Test;
 public class Solution {
     @Test
     public void test() {
-        System.out.println(isInterleave("dbbcc","aabcc", "aadbbcbccc"));
+        System.out.println(isInterleave("dbbcc", "aabcc", "aadbbcbccc"));
     }
 
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        return true;
+        int n = s1.length(), m = s2.length(), t = s3.length();
+
+        if (n + m != t) {
+            return false;
+        }
+
+        boolean[][] f = new boolean[n + 1][m + 1];
+
+        f[0][0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[i][j] = f[i][j] || (f[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+                if (j > 0) {
+                    f[i][j] = f[i][j] || (f[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+
+        return f[n][m];
     }
 }
