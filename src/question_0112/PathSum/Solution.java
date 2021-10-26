65affce0fb80b21a441fd1c7879c58c6b551cd74
Jungle <1198581227@qq.com>
2021-10-26 21:38:34 +0800

    0112-递归

---
+++ src/question_0112/PathSum/Solution.java
@@ -0,0 +1,32 @@
+package question_0112.PathSum;
+
+import other.TreeNode;
+
+/**
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ * <p>
+ * A leaf is a node with no children.
+ * <p>
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ * <p>
+ * 叶子节点 是指没有子节点的节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/10/26 21:26
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.right == null && root.left == null) {
+            return targetSum == root.val;
+        }
+        // 边减边下沉
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
