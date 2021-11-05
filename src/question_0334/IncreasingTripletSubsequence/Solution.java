4a95f6a6d07e59fcf331c32b455cd5b326bbd4df
Jungle <1198581227@qq.com>
2021-11-05 23:12:44 +0800

    0334-添加正解，以及那个非常鬼畜的测试用例

--- src/question_0334/IncreasingTripletSubsequence/Solution.java
+++ src/question_0334/IncreasingTripletSubsequence/Solution.java
@@ -24,16 +24,18 @@ public class Solution {
 
 
     public boolean increasingTriplet(int[] nums) {
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                if (nums[i] >= nums[j]) {
-                    continue;
-                }
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[j] < nums[k]) {
-                        return true;
-                    }
-                }
+        if (nums.length < 3) {
+            return false;
+        }
+        int small = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= small) {
+                small = num;
+            } else if (num <= mid) {
+                mid = num;
+            } else {
+                return true;
             }
         }
         return false;
