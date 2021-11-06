55d9e35ced5b7fee4dcf7455d816dd63f733faff
Jungle <1198581227@qq.com>
2021-11-06 22:33:23 +0800

    0187-修改实现

--- src/question_0187/RepeatedDNASequences/Solution.java
+++ src/question_0187/RepeatedDNASequences/Solution.java
@@ -36,8 +36,8 @@ public class Solution {
 
 
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans = new ArrayList<String>();
-        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        List<String> ans = new ArrayList<>();
+        Map<String, Integer> cnt = new HashMap<>();
         int n = s.length();
         for (int i = 0; i <= n - 10; ++i) {
             String sub = s.substring(i, i + 10);
