7aa82bd06d75e302febe429518ed5f815a11960d
Jungle <1198581227@qq.com>
2021-11-06 23:06:02 +0800

    0043-思路与其一致，模拟竖式解答，但我没写出来代码

---
+++ src/question_0043/MultiplyStrings/Solution.java
@@ -0,0 +1,50 @@
+package question_0043.MultiplyStrings;
+
+import org.junit.Test;
+
+/**
+ * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+ * <p>
+ * Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/multiply-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/11/6 22:39
+ */
+public class Solution {
+
+    @Test
+    public void test() {
+        System.out.println(multiply("1234", "9541"));
+    }
+
+
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        // 两数成绩的位数，最多为两数位数的和
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                // 前一位的进位
+                int k = res[i + j + 1];
+                int sum = (k + n1 * n2);
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) continue;
+            result.append(res[i]);
+        }
+        return result.toString();
+    }
+}
