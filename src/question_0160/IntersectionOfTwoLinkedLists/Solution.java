70782bd153d13fe43620c5ea787beddacd7306a6
Jungle <1198581227@qq.com>
2021-11-09 21:37:22 +0800

    0160-最常规的解法

---
+++ src/question_0160/IntersectionOfTwoLinkedLists/Solution.java
@@ -0,0 +1,41 @@
+package question_0160.IntersectionOfTwoLinkedLists;
+
+import other.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+ * <p>
+ * For example, the following two linked lists begin to intersect at node c1:
+ * <p>
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/11/9 21:17
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            set.add(headB);
+            headB = headB.next;
+        }
+        return null;
+    }
+}
