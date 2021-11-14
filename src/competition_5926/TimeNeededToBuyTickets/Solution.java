f47a95ed7043b04e906af1b28b22666af6a63c9e
Jungle <1198581227@qq.com>
2021-11-14 22:22:42 +0800

    5926-已解

---
+++ src/competition_5926/TimeNeededToBuyTickets/Solution.java
@@ -0,0 +1,44 @@
+package competition_5926.TimeNeededToBuyTickets;
+
+/**
+ * There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
+ * <p>
+ * You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
+ * <p>
+ * Each person takes exactly 1 second to buy a ticket. A person can only buy 1 ticket at a time and has to go back to the end of the line (which happens instantaneously) in order to buy more tickets. If a person does not have any tickets left to buy, the person will leave the line.
+ * <p>
+ * Return the time taken for the person at position k (0-indexed) to finish buying tickets.
+ * <p>
+ *  
+ * 有 n 个人前来排队买票，其中第 0 人站在队伍 最前方 ，第 (n - 1) 人站在队伍 最后方 。
+ * <p>
+ * 给你一个下标从 0 开始的整数数组 tickets ，数组长度为 n ，其中第 i 人想要购买的票数为 tickets[i] 。
+ * <p>
+ * 每个人买票都需要用掉 恰好 1 秒 。一个人 一次只能买一张票 ，如果需要购买更多票，他必须走到  队尾 重新排队（瞬间 发生，不计时间）。如果一个人没有剩下需要买的票，那他将会 离开 队伍。
+ * <p>
+ * 返回位于位置 k（下标从 0 开始）的人完成买票需要的时间（以秒为单位）。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/time-needed-to-buy-tickets
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/11/14 22:21
+ */
+public class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int rounds = 0;
+        while (tickets[k] > 0) {
+            for (int i = 0; i < tickets.length; i++) {
+                if (tickets[i] > 0) {
+                    rounds++;
+                    tickets[i] -= 1;
+                    if (tickets[k] == 0) {
+                        break;
+                    }
+                }
+            }
+        }
+        return rounds;
+    }
+}
