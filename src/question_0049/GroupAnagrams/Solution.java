ec38d255bc248053c6990ad97baa0fed905c8510
Jungle <1198581227@qq.com>
2021-11-15 22:50:24 +0800

    0049-我真以为有什么算法在里面-可惜仅仅是常规的思路。常规到我有点汗颜，去看了答案，然后发现答案也是这么写的

---
+++ src/question_0049/GroupAnagrams/Solution.java
@@ -0,0 +1,33 @@
+package question_0049.GroupAnagrams;
+
+import java.util.*;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * <p>
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母都恰好只用一次。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/11/15 22:37
+ */
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> temp = map.getOrDefault(key, new ArrayList<>());
+            temp.add(str);
+            map.put(key, temp);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
