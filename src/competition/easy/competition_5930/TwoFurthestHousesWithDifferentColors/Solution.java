54b25ea2381e5322fc7fd94c930d927be1abf597
Jungle <1198581227@qq.com>
2021-11-21 15:07:05 +0800

    调整部分题目位置

--- src/competition/easy/competition_5930/TwoFurthestHousesWithDifferentColors/Solution.java
+++ src/competition/easy/competition_5930/TwoFurthestHousesWithDifferentColors/Solution.java
@@ -1,4 +1,4 @@
-package competition.competition_5930.TwoFurthestHousesWithDifferentColors;
+package competition.easy.competition_5930.TwoFurthestHousesWithDifferentColors;
 
 /**
  * There are n houses evenly lined up on the street, and each house is beautifully painted. You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
