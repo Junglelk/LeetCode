a7bc0222ac4ee42736995859ee7376e83b0dfd69
Jungle <1198581227@qq.com>
2021-11-23 22:11:30 +0800

    0238-重写-这思路也很厉害

--- src/normal/medium/question_0238/ProductOfArrayExceptSelf/Solution.java
+++ src/normal/medium/question_0238/ProductOfArrayExceptSelf/Solution.java
@@ -1,4 +1,4 @@
-package normal.question_0238.ProductOfArrayExceptSelf;
+package normal.medium.question_0238.ProductOfArrayExceptSelf;
 
 import org.junit.Test;
 
@@ -61,20 +61,21 @@ public class Solution {
      */
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] target = new int[n];
+        int[] result = new int[n];
         int[] left = new int[n];
         int[] right = new int[n];
-        right[n - 1] = 1;
         left[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
+        right[n - 1] = 1;
+        for (int i = 1; i < left.length; i++) {
             left[i] = left[i - 1] * nums[i - 1];
         }
-        for (int i = nums.length - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * nums[i + 1];
+        for (int j = right.length - 2; j >= 0; j--) {
+            right[j] = right[j + 1] * nums[j + 1];
         }
-        for (int i = 0; i < target.length; i++) {
-            target[i] = left[i] * right[i];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = left[i] * right[i];
         }
-        return target;
+
+        return result;
     }
 }
