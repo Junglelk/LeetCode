98f97c123fd53859862b528d36f9919ecaa68371
Jungle <1198581227@qq.com>
2021-12-02 22:59:57 +0800

    0155-funny...?

---
+++ src/normal/easy/question_0155/MinStack/Solution.java
@@ -0,0 +1,93 @@
+package normal.easy.question_0155.MinStack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * <p>
+ * Implement the MinStack class:
+ * <pre>
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ * </pre>
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <pre>
+ * push(x) —— 将元素 x 推入栈中。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ * </pre>
+ * <pre>
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ *
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * </pre>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/12/2 22:38
+ */
+public class Solution {
+
+}
+
+/**
+ * 这题不要求确切返回出栈元素，所以，实际上就是维护最小值而已...
+ * 需要注意的是，虽然实际上并不需要真的出栈，但pop()操作还是会将原最小值踢出去，所以要额外维护。
+ * 原本以为这题需要自己实现栈...这题解也是挺无奈的...
+ */
+class MinStack {
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
