0af955e8d97feabc4c1702f1be32d357ba03994f
Jungle <1198581227@qq.com>
2021-12-03 22:35:02 +0800

    1823-牛逼，叹服

---
+++ src/normal/medium/question_1823/FindTheWinnerOfTheCircularGame/Solution.java
@@ -0,0 +1,65 @@
+package normal.medium.question_1823.FindTheWinnerOfTheCircularGame;
+
+import org.junit.Test;
+
+/**
+ * There are n friends that are playing a game. The friends are sitting in a circle and are numbered from 1 to n in clockwise order. More formally, moving clockwise from the ith friend brings you to the (i+1)th friend for 1 <= i < n, and moving clockwise from the nth friend brings you to the 1st friend.
+ * <p>
+ * The rules of the game are as follows:
+ * <pre>
+ * Start at the 1st friend.
+ * Count the next k friends in the clockwise direction including the friend you started at. The counting wraps around the circle and may count some friends more than once.
+ * The last friend you counted leaves the circle and loses the game.
+ * If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
+ * Else, the last friend in the circle wins the game.
+ * Given the number of friends, n, and an integer k, return the winner of the game.
+ * </pre>
+ * <p>
+ * 共有 n 名小伙伴一起做游戏。小伙伴们围成一圈，按 顺时针顺序 从 1 到 n 编号。确切地说，从第 i 名小伙伴顺时针移动一位会到达第 (i+1) 名小伙伴的位置，其中 1 <= i < n ，从第 n 名小伙伴顺时针移动一位会回到第 1 名小伙伴的位置。
+ * <p>
+ * 游戏遵循如下规则：
+ *
+ * <pre>
+ * 从第 1 名小伙伴所在位置 开始 。
+ * 沿着顺时针方向数 k 名小伙伴，计数时需要 包含 起始时的那位小伙伴。逐个绕圈进行计数，一些小伙伴可能会被数过不止一次。
+ * 你数到的最后一名小伙伴需要离开圈子，并视作输掉游戏。
+ * 如果圈子中仍然有不止一名小伙伴，从刚刚输掉的小伙伴的 顺时针下一位 小伙伴 开始，回到步骤 2 继续执行。
+ * 否则，圈子中最后一名小伙伴赢得游戏。
+ * 给你参与游戏的小伙伴总数 n ，和一个整数 k ，返回游戏的获胜者。
+ * </pre>
+ * <pre>
+ * 输入：n = 5, k = 2
+ * 输出：3
+ * 解释：游戏运行步骤如下：
+ * 1) 从小伙伴 1 开始。
+ * 2) 顺时针数 2 名小伙伴，也就是小伙伴 1 和 2 。
+ * 3) 小伙伴 2 离开圈子。下一次从小伙伴 3 开始。
+ * 4) 顺时针数 2 名小伙伴，也就是小伙伴 3 和 4 。
+ * 5) 小伙伴 4 离开圈子。下一次从小伙伴 5 开始。
+ * 6) 顺时针数 2 名小伙伴，也就是小伙伴 5 和 1 。
+ * 7) 小伙伴 1 离开圈子。下一次从小伙伴 3 开始。
+ * 8) 顺时针数 2 名小伙伴，也就是小伙伴 3 和 5 。
+ * 9) 小伙伴 5 离开圈子。只剩下小伙伴 3 。所以小伙伴 3 是游戏的获胜者。
+ * </pre>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-the-winner-of-the-circular-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/12/3 22:10
+ */
+public class Solution {
+    @Test
+    public void test() {
+        System.out.println(findTheWinner(5, 2));
+    }
+
+
+    public int findTheWinner(int n, int k) {
+        int dp = 0;
+        for (int i = 2; i <= n; i++) {
+            dp = ((dp + k) % i);
+        }
+        return dp + 1;
+    }
+}
