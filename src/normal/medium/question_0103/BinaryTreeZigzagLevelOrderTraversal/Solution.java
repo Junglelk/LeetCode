a37c2b1dd09bf600ee7fd18aeec17f0eaa72c93c
Jungle <1198581227@qq.com>
2021-12-04 23:26:29 +0800

    0103-犯了一个层序遍历时的常见错误。补充了一些说明

--- src/normal/medium/question_0103/BinaryTreeZigzagLevelOrderTraversal/Solution.java
+++ src/normal/medium/question_0103/BinaryTreeZigzagLevelOrderTraversal/Solution.java
@@ -33,18 +33,34 @@ import java.util.Queue;
  * @since 2021/12/4 22:47
  */
 public class Solution {
+    /**
+     * 队列大小不应当作为遍历时的比较条件，因为大小是动态变化的。<br>
+     * 这题的基本思路与我一开始一致，但是为什么没能将一开始的思路转换为代码呢？；<br>
+     * 一方面是忘记了双端队列的一些特征，一直以来都仅仅是将其视为栈的替代品；也就是说，基础不够牢靠。<br>
+     * 另一方面，今晚想要冲击到200道题，有些急了。好在确实能独立写出来这个代码，要对今晚的思想引以为戒。<br>
+     *
+     * @param root 根节点
+     * @return 目标列表
+     */
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> ret = new LinkedList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         // 显然第一层左到右，第二层右到左。后面奇偶性与遍历方式一致
         // 第一层左到右
         boolean order = true;
         while (!queue.isEmpty()) {
             Deque<Integer> list = new LinkedList<>();
-            for (int i = 0; i < queue.size(); i++) {
+            // 犯了一个很老土的错误
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode temp = queue.poll();
-                //
+                // 当为奇数层时，从左往右，即每次将数值插入到双端队列的最新位置，基于队列的先进先出，
+                // 就是从左到右的顺序。偶数层与之相反
+                assert temp != null;
                 if (order) {
                     list.offerLast(temp.val);
                 } else {
