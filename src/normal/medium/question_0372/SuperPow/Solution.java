7d9f2698ce2545cf186f4697cbb6db4cd649bbc9
Jungle <1198581227@qq.com>
2021-12-05 20:36:50 +0800

    0372-完整版

--- src/normal/medium/question_0372/SuperPow/Solution.java
+++ src/normal/medium/question_0372/SuperPow/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
     public int superPow(int a, int[] b) {
         int ans = 1;
         for (int i = b.length - 1; i >= 0; --i) {
-            ans = ans * pow(a, b[i]);
+            ans = (int) ((long) ans * pow(a, b[i]) % MOD);
             a = pow(a, 10);
         }
         return ans;
@@ -30,9 +30,9 @@ public class Solution {
         int res = 1;
         while (n != 0) {
             if (n % 2 != 0) {
-                res = res * x;
+                res = (int) ((long) res * x % MOD);
             }
-            x = x * x;
+            x = (int) ((long) x * x % MOD);
             n /= 2;
         }
         return res;
