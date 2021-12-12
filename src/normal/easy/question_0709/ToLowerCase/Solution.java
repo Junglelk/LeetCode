8d1239d2312b8c489bc0ce1fc7c5d2bc71895805
Jungle <1198581227@qq.com>
2021-12-12 09:54:41 +0800

    0709-Api战士不请自来

---
+++ src/normal/easy/question_0709/ToLowerCase/Solution.java
@@ -0,0 +1,17 @@
+package normal.easy.question_0709.ToLowerCase;
+
+/**
+ * Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+ *
+ * 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/to-lower-case
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Jungle
+ * @since 2021/12/12 9:53
+ */
+public class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
