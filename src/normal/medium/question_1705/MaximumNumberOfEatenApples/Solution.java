3c79bc647ec98596306065334132a61d80869eb8
Jungle <1198581227@qq.com>
2021-12-24 23:01:56 +0800

    1705-明天加班，就这了

---
+++ src/normal/medium/question_1705/MaximumNumberOfEatenApples/Solution.java
@@ -0,0 +1,56 @@
+package normal.medium.question_1705.MaximumNumberOfEatenApples;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * There is a special kind of apple tree that grows apples every day for n days. On the ith day, the tree grows apples[i] apples that will rot after days[i] days, that is on day i + days[i] the apples will be rotten and cannot be eaten. On some days, the apple tree does not grow any apples, which are denoted by apples[i] == 0 and days[i] == 0.
+ * <p>
+ * You decided to eat at most one apple a day (to keep the doctors away). Note that you can keep eating after the first n days.
+ * <p>
+ * Given two integer arrays days and apples of length n, return the maximum number of apples you can eat.
+ * <p>
+ * 有一棵特殊的苹果树，一连 n 天，每天都可以长出若干个苹果。在第 i 天，树上会长出 apples[i] 个苹果，这些苹果将会在 days[i] 天后（也就是说，第 i + days[i] 天时）腐烂，变得无法食用。也可能有那么几天，树上不会长出新的苹果，此时用 apples[i] == 0 且 days[i] == 0 表示。
+ * <p>
+ * 你打算每天 最多 吃一个苹果来保证营养均衡。注意，你可以在这 n 天之后继续吃苹果。
+ * <p>
+ * 给你两个长度为 n 的整数数组 days 和 apples ，返回你可以吃掉的苹果的最大数目。
+ * <pre>
+ * 输入：apples = [1,2,3,5,2], days = [3,2,1,4,2]
+ * 输出：7
+ * 解释：你可以吃掉 7 个苹果：
+ * - 第一天，你吃掉第一天长出来的苹果。
+ * - 第二天，你吃掉一个第二天长出来的苹果。
+ * - 第三天，你吃掉一个第二天长出来的苹果。过了这一天，第三天长出来的苹果就已经腐烂了。
+ * - 第四天到第七天，你吃的都是第四天长出来的苹果。
+ * </pre>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-number-of-eaten-apples
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2021/12/24 21:28
+ */
+public class Solution {
+    public int eatenApples(int[] apples, int[] days) {
+        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        int n = apples.length, time = 0, ans = 0;
+        while (time < n || !q.isEmpty()) {
+            if (time < n && apples[time] > 0) {
+                q.add(new int[]{time + days[time] - 1, apples[time]});
+            }
+            while (!q.isEmpty() && (q.peek()[0] < time)) {
+                q.poll();
+            }
+            if (!q.isEmpty()) {
+                int[] cur = q.poll();
+                if (--cur[1] > 0 && cur[0] > time) {
+                    q.add(cur);
+                }
+                ans++;
+            }
+            time++;
+        }
+        return ans;
+    }
+}
