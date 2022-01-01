f6e87f208b16769420854405d8820044177653b2
Jungle <1198581227@qq.com>
2022-01-01 15:11:34 +0800

    2022-2022年的第一道题

---
+++ src/normal/easy/question_2022/Convert1DArrayInto2DArray/Solution.java
@@ -0,0 +1,35 @@
+package normal.easy.question_2022.Convert1DArrayInto2DArray;
+
+/**
+ * You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with m rows and n columns using all the elements from original.
+ * <p>
+ * The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
+ * <p>
+ * Return an m x n 2D array constructed according to the above procedure, or an empty 2D array if it is impossible.
+ * <p>
+ * 给你一个下标从 0 开始的一维整数数组 original 和两个整数 m 和  n 。你需要使用 original 中 所有 元素创建一个 m 行 n 列的二维数组。
+ * <p>
+ * original 中下标从 0 到 n - 1 （都 包含 ）的元素构成二维数组的第一行，下标从 n 到 2 * n - 1 （都 包含 ）的元素构成二维数组的第二行，依此类推。
+ * <p>
+ * 请你根据上述过程返回一个 m x n 的二维数组。如果无法构成这样的二维数组，请你返回一个空的二维数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/convert-1d-array-into-2d-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/1/1 15:09
+ */
+public class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[0][];
+        }
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < original.length; i += n) {
+            System.arraycopy(original, i, ans[i / n], 0, n);
+        }
+        return ans;
+    }
+
+}
