52ec6eca5a64567689c1964a9f93eacab81f05b4
Jungle <1198581227@qq.com>
2022-01-29 10:09:24 +0800

    0222-很常规的二叉树的层序遍历，但没有用到“完全二叉树”的性质，不是个好的解答

---
+++ src/normal/medium/question_0222/CountCompleteTreeNodes/Solution.java
@@ -0,0 +1,54 @@
+package normal.medium.question_0222.CountCompleteTreeNodes;
+
+import other.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Given the root of a complete binary tree, return the number of the nodes in the tree.
+ * <p>
+ * According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+ * <p>
+ * Design an algorithm that runs in less than O(n) time complexity.
+ * <p>
+ * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+ * <p>
+ * 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，
+ * <p>
+ * 其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1 ~ 2^h 个节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-complete-tree-nodes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/1/29 10:04
+ */
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                count++;
+                assert poll != null;
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+
+        }
+        return count;
+
+    }
+}
