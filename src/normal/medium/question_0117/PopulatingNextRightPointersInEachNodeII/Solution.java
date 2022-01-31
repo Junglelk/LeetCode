97797a875ce1aded1cd0ae9232c7fdf5baeb1de8
jungle <1198581227@qq.com>
2022-01-31 17:33:39 +0800

    0117-和116完全一样啊

---
+++ src/normal/medium/question_0117/PopulatingNextRightPointersInEachNodeII/Solution.java
@@ -0,0 +1,93 @@
+package normal.medium.question_0117.PopulatingNextRightPointersInEachNodeII;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Given a binary tree
+ * <p>
+ * <pre>
+ * struct Node {
+ *  int val;
+ *  Node *left;
+ *  Node *right;
+ *  Node *next;
+ * }
+ * </pre>
+ * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+ * <p>
+ * Initially, all next pointers are set to NULL.
+ * <p>
+ * 给定一个二叉树
+ * <pre>
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * </pre>
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/01/31 17:27
+ */
+public class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node poll = queue.poll();
+                if (i < size - 1) {
+                    poll.next = queue.peek();
+                }
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+        }
+        return root;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
