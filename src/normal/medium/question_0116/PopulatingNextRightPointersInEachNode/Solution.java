0da427575329e814f74735f4bf7208c649fcb825
jungle <1198581227@qq.com>
2022-01-31 17:32:52 +0800

    0116-补充题目

--- src/normal/medium/question_0116/PopulatingNextRightPointersInEachNode/Solution.java
+++ src/normal/medium/question_0116/PopulatingNextRightPointersInEachNode/Solution.java
@@ -4,6 +4,36 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
+ * You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+ * <pre>
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * </pre>
+ * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+ * <p>
+ * Initially, all next pointers are set to NULL.
+ * <p>
+ * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * <pre>
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * </pre>
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author Jungle
  * @since 2022/01/30 11:21
  */
