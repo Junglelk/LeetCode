ea6d94203007c89c7146a1770ae1d7d3263966c7
jungle <1198581227@qq.com>
2022-02-01 18:25:47 +0800

    面试金典-旋转90°，先上下，再主对角线左右翻转

---
+++ src/interview/ch01/question_07/RotateMatrix/Solution.java
@@ -0,0 +1,35 @@
+package interview.ch01.question_07.RotateMatrix;
+
+/**
+ * Given an image represented by an N x N matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+ * <p>
+ * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+ * <p>
+ * 不占用额外内存空间能否做到？
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-matrix-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/02/01 18:15
+ */
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int start = 0;
+        int end = matrix.length - 1;
+        while (start < end) {
+            int[] temp = matrix[start];
+            matrix[start] = matrix[end];
+            matrix[end] = temp;
+            start++;
+            end--;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
