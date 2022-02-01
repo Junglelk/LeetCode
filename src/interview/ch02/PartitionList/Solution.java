d1968250c41c53d18e2bd9a34167003a5d34e62f
jungle <1198581227@qq.com>
2022-02-01 19:37:50 +0800

    面试金典-ch02-04-一次编写，忘记补全边界条件，所以两次ac

---
+++ src/interview/ch02/PartitionList/Solution.java
@@ -0,0 +1,51 @@
+package interview.ch02.PartitionList;
+
+import other.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Write code to partition a linked list around a value x,
+ * <p>
+ * such that all nodes less than x come before all nodes greater than or equal to x.
+ * <p>
+ * If x is contained within the list, the values of x only need to be after the elements less than x (see below). The partition element x can appear anywhere in the "right partition"; it does not need to appear between the left and right partitions.
+ * <p>
+ * 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+ * <p>
+ * 你不需要 保留 每个分区中各节点的初始相对位置
+ * <pre>
+ * 输入：head = [1,4,3,2,5,2], x = 3
+ * 输出：[1,2,2,4,3,5]
+ * </pre>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/partition-list-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/02/01 19:20
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        List<Integer> list = new ArrayList<>();
+        list.add(head.val);
+        while (head.next != null) {
+            head = head.next;
+            list.add(head.val);
+        }
+        Integer[] array = list.toArray(new Integer[]{});
+        Arrays.sort(array);
+        ListNode node = new ListNode();
+        head = node;
+        for (int i = 0; i < array.length; i++) {
+            node.next = new ListNode(array[i]);
+            node = node.next;
+        }
+        return head.next;
+    }
+}
