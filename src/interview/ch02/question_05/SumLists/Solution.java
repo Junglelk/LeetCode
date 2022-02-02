ec54e5f8cf835e3072d1ca08ade834ed2ae953e1
jungle <1198581227@qq.com>
2022-02-02 22:25:19 +0800

    temp

---
+++ src/interview/ch02/question_05/SumLists/Solution.java
@@ -0,0 +1,25 @@
+package interview.ch02.question_05.SumLists;
+
+import other.ListNode;
+
+/**
+ * You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+ * <p>
+ * 给定两个用链表表示的整数，每个节点包含一个数位。
+ * <p>
+ * 这些数位是反向存放的，也就是个位排在链表首部。
+ * <p>
+ * 编写函数对这两个整数求和，并用链表形式返回结果。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-lists-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/02/02 22:24
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
