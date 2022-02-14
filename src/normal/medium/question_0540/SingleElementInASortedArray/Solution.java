07da07edd0d33dbf2d45f7f45ba0e0c5e4d72be3
Jungle <1198581227@qq.com>
2022-02-14 21:49:30 +0800

    0540-这是一道简单题的拓展-其实要求logN的时间复杂度，这个写法是N

---
+++ src/normal/medium/question_0540/SingleElementInASortedArray/Solution.java
@@ -0,0 +1,31 @@
+package normal.medium.question_0540.SingleElementInASortedArray;
+
+/**
+ * You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+ * <p>
+ * Return the single element that appears only once.
+ * <p>
+ * Your solution must run in O(log n) time and O(1) space.
+ * <p>
+ * 给你一个仅由整数组成的有序数组，其中每个元素都会出现两次，唯有一个数只会出现一次。
+ * <p>
+ * 请你找出并返回只出现一次的那个数。
+ * <p>
+ * 你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-element-in-a-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/2/14 21:22
+ */
+public class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int ret = 0;
+        for (int num : nums) {
+            ret ^= num;
+        }
+        return ret;
+    }
+}
