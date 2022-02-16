05d791349edbffb0eb36301a21c13f43db1c85e4
Jungle <1198581227@qq.com>
2022-02-16 23:42:56 +0800

    0031-错解，有很多边界条件未处理

---
+++ src/normal/medium/question_0031/NextPermutation/Solution.java
@@ -0,0 +1,66 @@
+package normal.medium.question_0031.NextPermutation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+ * <pre>
+ * For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+ * </pre>
+ * The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+ * <pre>
+ * For example, the next permutation of arr = [1,2,3] is [1,3,2].
+ * Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+ * While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+ * </pre>
+ * Given an array of integers nums, find the next permutation of nums.
+ * <p>
+ * The replacement must be in place and use only constant extra memory.
+ * <p>
+ * 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+ * <pre>
+ * 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+ * </pre>
+ * 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，
+ * <p>
+ * 那么数组的 下一个排列 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+ * <pre>
+ * 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+ * 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+ * 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+ * </pre>
+ * 给你一个整数数组 nums ，找出 nums 的下一个排列。
+ * <p>
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-permutation
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/2/16 23:21
+ */
+public class Solution {
+    @Test
+    public void test() {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,12};
+        nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+
+
+    public void nextPermutation(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+                return;
+            }
+        }
+        Arrays.sort(nums);
+    }
+}
