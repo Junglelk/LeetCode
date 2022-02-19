3f21b49ab62631abfe156ff787e8010cd9c9703f
Jungle <1198581227@qq.com>
2022-02-19 15:19:22 +0800

    0033-有序+查找=二分，目前的解法没有用到二分

---
+++ src/normal/medium/question_0033/SearchInRotatedSortedArray/Solution.java
@@ -0,0 +1,34 @@
+package normal.medium.question_0033.SearchInRotatedSortedArray;
+
+/**
+ * There is an integer array nums sorted in ascending order (with distinct values).
+ * <p>
+ * Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+ * <p>
+ * Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ * <p>
+ * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/2/19 15:02
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
