5f6fd29e8c0000f9b299630d87eee5ef5a2aeecf
Jungle <1198581227@qq.com>
2022-02-19 19:16:45 +0800

    0033-正解

--- src/normal/medium/question_0033/SearchInRotatedSortedArray/Solution.java
+++ src/normal/medium/question_0033/SearchInRotatedSortedArray/Solution.java
@@ -33,21 +33,33 @@ public class Solution {
 
 
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        return binary(nums, left, right, target);
-    }
-
-    int binary(int[] nums, int left, int right, int target) {
-        int mid = left / 2 + right / 2;
-        if (nums[mid] != target && mid != left && mid != right) {
-            int l = binary(nums, mid + 1, right, target);
-            int r = binary(nums, left, mid - 1, target);
-            return l == -1 ? r : l;
-        } else if (nums[mid] == target) {
-            return mid;
-        } else {
+        if (nums.length == 0) {
             return -1;
         }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && nums[nums.length - 1] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 }
