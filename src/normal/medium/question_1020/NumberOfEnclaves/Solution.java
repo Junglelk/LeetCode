4f9020ce770aa74f60f4e98f9760642f2fe6d536
Jungle <1198581227@qq.com>
2022-03-05 19:26:03 +0800

    1020-dfs-先对边界进行dfs，将与边界接壤的地块置0，最后统计剩余的格子中剩余的1即为答案

--- src/normal/medium/question_1020/NumberOfEnclaves/Solution.java
+++ src/normal/medium/question_1020/NumberOfEnclaves/Solution.java
@@ -37,47 +37,62 @@ public class Solution {
     @Test
     public void test() {
         System.out.println(numEnclaves(new int[][]{
-                {0, 0, 0, 0},
+                {0, 0, 1, 0},
                 {1, 0, 1, 0},
                 {0, 1, 1, 0},
                 {0, 0, 0, 0}
         }));
     }
 
+    int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (grid[0][i] == 1) {
+                dfs(grid, 0, i);
+            }
+            if (grid[m - 1][i] == 1) {
+                dfs(grid, m - 1, i);
+            }
+        }
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+            if (grid[i][0] == 1) {
+                dfs(grid, i, 0);
+            }
+            if (grid[i][n - 1] == 1) {
+                dfs(grid, i, n - 1);
+            }
+        }
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
                 if (grid[i][j] == 1) {
-                    if (dfs(grid, i, j)) {
-                        ret++;
-                    }
+                    ret++;
                 }
             }
         }
+
         return ret;
     }
 
-    int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    private boolean dfs(int[][] grid, int i, int j) {
-        // 越界，说明遍历到了边界
-
-
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+            return;
+        }
+        grid[i][j] = 0;
         for (int[] dir : dirs) {
+
             int ni = i + dir[0];
             int nj = j + dir[1];
-            if (ni < 0 || ni >= grid.length || nj < 0 || nj >= grid[0].length) {
-                return false;
+            if (ni >= 0 && ni < grid.length && nj >= 0 && nj < grid[0].length) {
+                if (grid[ni][nj] == 1) {
+                    dfs(grid, ni, nj);
+                }
             }
-            dfs(grid, ni, nj);
         }
 
-
-        return true;
     }
 
 
