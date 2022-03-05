9187c1ccaad0dfe3cd2c38c4aed9c3b48a82dd8d
Jungle <1198581227@qq.com>
2022-03-05 19:47:36 +0800

    1254-dfs-初解-错解

--- src/normal/medium/question_1254/NumberOfClosedIslands/Solution.java
+++ src/normal/medium/question_1254/NumberOfClosedIslands/Solution.java
@@ -1,5 +1,7 @@
 package normal.medium.question_1254.NumberOfClosedIslands;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally (all left, top, right, bottom) surrounded by 1s.
  * <p>
@@ -26,34 +28,47 @@ package normal.medium.question_1254.NumberOfClosedIslands;
  * @since 2022/3/3 16:53
  */
 public class Solution {
+
+
+    @Test
+    public void test() {
+        System.out.println(closedIsland(new int[][]{
+                {1, 1, 1, 1, 1, 1, 1, 0},
+                {1, 0, 0, 0, 0, 1, 1, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0},
+                {1, 0, 0, 0, 0, 1, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 0}
+        }));
+    }
+
+
     public int closedIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int maxArea = Integer.MIN_VALUE;
+        int ret = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    int temp = dfs(grid, i, j);
-                    maxArea = Math.max(maxArea, temp);
+                if (i > 0 && i < m - 1 && j > 0 && j < n - 1 && grid[i][j] == 0) {
+                    dfs(grid, i, j);
+                    ret++;
                 }
             }
         }
-        return Math.max(maxArea, 0);
+        return ret;
     }
 
     int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    private int dfs(int[][] grid, int i, int j) {
+    private void dfs(int[][] grid, int i, int j) {
         if (grid == null || grid.length == 0) {
-            return 0;
+            return;
         }
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 1) {
-            return 0;
+            return;
         }
         grid[i][j] = 1;
         for (int[] dir : dirs) {
-
+            dfs(grid, i + dir[0], j + dir[1]);
         }
-        return area;
     }
 }
