9feaaccc1b798bdd5a351b35ca628d5f53a546b1
Jungle <1198581227@qq.com>
2022-03-09 22:56:17 +0800

    0077-初解-错解

---
+++ src/normal/medium/question_0077/Combinations/Solution.java
@@ -0,0 +1,75 @@
+package normal.medium.question_0077.Combinations;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+ * <p>
+ * You may return the answer in any order.
+ * <p>
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * <pre>
+ * 输入：n = 4, k = 2
+ * 输出：
+ * [
+ *   [2,4],
+ *   [3,4],
+ *   [2,3],
+ *   [1,2],
+ *   [1,3],
+ *   [1,4],
+ * ]
+ * </pre>
+ * 你可以按 任何顺序 返回答案。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combinations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/3/9 22:24
+ */
+public class Solution {
+
+    @Test
+    public void test() {
+        System.out.println(combine(3, 3));
+    }
+
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        if (k == n) {
+            for (int i = 1; i < n; i++) {
+                int i1 = i;
+                ans.add(new ArrayList<>(new ArrayList<>() {{
+                    add(i1);
+                }}));
+            }
+        }
+        dfs(1, ans, temp, n, k);
+        return ans;
+    }
+
+    private void dfs(int start, List<List<Integer>> ans, List<Integer> temp, int n, int k) {
+        if (temp.size() == k) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            temp.add(i);
+            for (int j = start + 1; j <= n; j++) {
+                if (i >= j) {
+                    continue;
+                }
+                temp.add(j);
+                dfs(j, ans, temp, n, k);
+                temp.remove(temp.size() - 1);
+            }
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
