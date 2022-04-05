f3d3e76bbc54d68819acd7f0029316f5a10f3f46
Jungle <1198581227@qq.com>
2022-04-05 18:58:32 +0800

    0176-medium

---
+++ src/normal/medium/question_0176/SecondHighestSalary/Solution.java
@@ -0,0 +1,40 @@
+package normal.medium.question_0176.SecondHighestSalary;
+
+/**
+ * <pre>
+ *  +-------------+------+
+ * | Column Name | Type |
+ * +-------------+------+
+ * | id          | int  |
+ * | salary      | int  |
+ * +-------------+------+
+ * id 是这个表的主键。
+ * 表的每一行包含员工的工资信息。
+ * </pre>
+ * 编写一个 SQL 查询，获取并返回 Employee 表中第二高的薪水 。如果不存在第二高的薪水，查询应该返回 null 。
+ * <pre>
+ * 输入：
+ * Employee 表：
+ * +----+--------+
+ * | id | salary |
+ * +----+--------+
+ * | 1  | 100    |
+ * | 2  | 200    |
+ * | 3  | 300    |
+ * +----+--------+
+ * 输出：
+ * +---------------------+
+ * | SecondHighestSalary |
+ * +---------------------+
+ * | 200                 |
+ * +---------------------+
+ * </pre>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/second-highest-salary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/4/5 18:46
+ */
+public class Solution {
+}
