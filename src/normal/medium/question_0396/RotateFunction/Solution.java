03404d5839f22a0aae3f76d592c095320d13374b
Jungle <1198581227@qq.com>
2022-04-22 23:48:59 +0800

    0396-疲惫

---
+++ src/normal/medium/question_0396/RotateFunction/Solution.java
@@ -0,0 +1,44 @@
+package normal.medium.question_0396.RotateFunction;
+
+import java.util.Arrays;
+
+/**
+ * You are given an integer array nums of length n.
+ * <p>
+ * Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
+ * <p>
+ * <li/>F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+ * Return the maximum value of F(0), F(1), ..., F(n-1).
+ * <p>
+ * The test cases are generated so that the answer fits in a 32-bit integer.
+ * <p>
+ * 给定一个长度为 n 的整数数组 nums 。
+ * <p>
+ * 假设 arrk 是数组 nums 顺时针旋转 k 个位置后的数组，我们定义 nums 的 旋转函数  F 为：
+ * <p>
+ * <li/>F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1]
+ * 返回 F(0), F(1), ..., F(n-1)中的最大值 。
+ * <p>
+ * 生成的测试用例让答案符合 32 位 整数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode-cn.com/problems/rotate-function">https://leetcode-cn.com/problems/rotate-function</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/4/22 23:47
+ */
+public class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int f = 0, n = nums.length, numSum = Arrays.stream(nums).sum();
+        for (int i = 0; i < n; i++) {
+            f += i * nums[i];
+        }
+        int res = f;
+        for (int i = n - 1; i > 0; i--) {
+            f += numSum - n * nums[i];
+            res = Math.max(res, f);
+        }
+        return res;
+    }
+}
