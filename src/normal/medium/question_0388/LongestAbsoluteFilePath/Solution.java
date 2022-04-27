fb0456b814198c84c57ddfe21b2d06d88be70104
Jungle <1198581227@qq.com>
2022-04-27 23:41:18 +0800

    0388-官解-我的思路总感觉缺乏系统性的思维，总在一些边边角角里面使劲，然后写出一坨代码，最后疯狂卡边界

--- src/normal/medium/question_0388/LongestAbsoluteFilePath/Solution.java
+++ src/normal/medium/question_0388/LongestAbsoluteFilePath/Solution.java
@@ -2,10 +2,6 @@ package normal.medium.question_0388.LongestAbsoluteFilePath;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-
 /**
  * @author Jungle
  * @since 2022/4/20 23:03
@@ -14,62 +10,45 @@ public class Solution {
     @Test
     public void test() {
         final String s = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
-        final String[] split = s.split("\n");
-        for (int i = 0; i < split.length; i++) {
-            split[i] = split[i].replace('\t', '\\');
-        }
-        System.out.println(Arrays.toString(split));
-        System.out.println(s);
         System.out.println(lengthLongestPath(s));
     }
 
     public int lengthLongestPath(String input) {
-        if (!input.contains(".")) {
-            return 0;
-        }
-        Deque<String> stack = new ArrayDeque<>();
-        int max = 0;
-        int length = 0;
-        final String[] split = input.split("\n");
-        for (int i = 0; i < split.length; i++) {
-            String path = split[i];
-            if (i == 0) {
-                stack.push(path);
-            } else {
-                if (path.contains(".")) {
-                    length += path.replaceAll("\t", "").length() + 1;
-                    max = Math.max(length, max);
-                } else {
-                    String peek = stack.peek();
-                    int num1 = hasT(peek);
-                    int num2 = hasT(path);
-                    while (num1 >= num2) {
-                        if (stack.size() > 1) {
-                            num1 = hasT(stack.pop());
-                        } else break;
-                    }
-                    stack.push(path);
+        int n = input.length();
+        int pos = 0;
+        int ans = 0;
+        int[] level = new int[n + 1];
+
+        while (pos < n) {
+//             检测当前文件的深度
+            int depth = 1;
+            while (pos < n && input.charAt(pos) == '\t') {
+                pos++;
+                depth++;
+            }
+//             统计当前文件名的长度
+            int len = 0;
+            boolean isFile = false;
+            while (pos < n && input.charAt(pos) != '\n') {
+                if (input.charAt(pos) == '.') {
+                    isFile = true;
                 }
-
+                len++;
+                pos++;
             }
+            // 跳过换行符
+            pos++;
 
-        }
-
-        return max;
-    }
-
-    int hasT(String s) {
-        if (s == null) {
-            return 0;
-        }
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '\\') {
-                num++;
+            if (depth > 1) {
+                len += level[depth - 1] + 1;
+            }
+            if (isFile) {
+                ans = Math.max(ans, len);
             } else {
-                break;
+                level[depth] = len;
             }
         }
-        return num;
+        return ans;
     }
+
 }
