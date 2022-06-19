6cf1843e616dc662e880728a7ab2f9b57cd0c86f
Jungle <1198581227@qq.com>
2022-06-19 23:17:41 +0800

    今天陪朋友，没啥时间，水一题easy

---
+++ src/normal/easy/question_0182/DuplicateEmails/Solution.java
@@ -0,0 +1,28 @@
+package normal.easy.question_0182.DuplicateEmails;
+
+/**
+ * Description:
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | id          | int     |
+ * | email       | varchar |
+ * +-------------+---------+
+ * id is the primary key column for this table.
+ * Each row of this table contains an email. The emails will not contain uppercase letters.
+ * <p>
+ * Write an SQL query to report all the duplicate emails.
+ * <p>
+ * Return the result table in any order.
+ * <p>
+ * The query result format is in the following example.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/duplicate-emails">duplicate-emails</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jungle
+ * @since 2022/6/19 23:08
+ */
+public class Solution {
+}
