974a1615b6c7c4c10b2e91917f8d8ad486278376
jungle <1198581227@qq.com>
2022-07-13 14:30:00 +0800

    一道SQL题，按理说，不用函数应该也能做吧...

---
+++ src/normal/medium/question_0178/RankScores/Solution.java
@@ -0,0 +1,25 @@
+package normal.medium.question_0178.RankScores;
+
+/**
+ * Write an SQL query to rank the scores. The ranking should be calculated according to the following rules:
+ * <p>
+ * <li/> The scores should be ranked from the highest to the lowest.
+ * <li/> If there is a tie between two scores, both should have the same ranking.
+ * <li/> After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+ * Return the result table ordered by score in descending order.
+ * <p>
+ * The query result format is in the following example.
+ * <p>
+ * 编写 SQL 查询对分数进行排序。排名按以下规则计算:
+ * <p>
+ * <li/> 分数应按从高到低排列。
+ * <li/> 如果两个分数相等，那么两个分数的排名应该相同。
+ * <li/> 在排名相同的分数后，排名数应该是下一个连续的整数。换句话说，排名之间不应该有空缺的数字。
+ * 按 score 降序返回结果表。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/rank-scores">rank-scores</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+}
