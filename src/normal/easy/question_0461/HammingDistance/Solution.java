e81ced0b5bc612c5ab23fd8543b2120757c41c51
jungle <1198581227@qq.com>
2022-07-18 10:00:54 +0800

    0461-api战士不请自来

---
+++ src/normal/easy/question_0461/HammingDistance/Solution.java
@@ -0,0 +1,16 @@
+package normal.easy.question_0461.HammingDistance;
+
+/**
+ * The <a href="https://en.wikipedia.org/wiki/Hamming_distance">Hamming distance</a> between two integers is the number of positions at which the corresponding bits are different.
+ * <p>
+ * Given two integers x and y, return the Hamming distance between them.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/hamming-distance">Hamming distance</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
